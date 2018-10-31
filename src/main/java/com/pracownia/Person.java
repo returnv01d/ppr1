8c48c802d0212c75f39c6e900f838df61812f662
returnv01d <bartlomiej.winiarski88@gmail.com>
2018-10-31 23:08:08 +0100

    Initial Person class.

---
+++ src/main/java/com/pracownia/Person.java
@@ -0,0 +1,38 @@
+package com.pracownia;
+
+public class Person {
+    String name;
+    String surname;
+    String PESEL;
+    String city;
+
+    public String getCity() {
+        return city;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public String getPESEL() {
+        return PESEL;
+    }
+
+    public void ChangeInformation(String newName, String newSurname, String newCity){
+        name = newName;
+        surname = newSurname;
+        city = newCity;
+    }
+
+    public Person(String name, String surname, String PESEL, String city){
+        this.name = name;
+        this.surname = surname;
+        this.PESEL = PESEL;
+        this.city = city;
+    }
+}
